016585ae6f0bed406f133cebaa01ec6a23de5526
Karthikganiga443 <karthikganiga443@gmail.com>
2025-12-05 12:44:09 +0530

    done

---
+++ src/main/java/com/employee/management/controller/EmployeeController.java
@@ -0,0 +1,72 @@
+package com.employee.management.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.employee.management.model.Employee;
+import com.employee.management.service.EmployeeService;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService empServ;
+
+    @GetMapping("/")
+    public String viewHomePage(Model model) {
+        return viewPage(1, model);
+    }
+
+    @GetMapping("/page/{pageNo}")
+    public String viewPage(@PathVariable int pageNo, Model model) {
+        int pageSize = 9;
+        Page<Employee> page = empServ.findPaginated(pageNo, pageSize);
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPage", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+        model.addAttribute("listEmployees", page.getContent());
+
+        return "index";
+    }
+
+    @GetMapping("/showNewEmpForm")
+    public String showNewEmpForm(Model model) {
+        model.addAttribute("employee", new Employee());
+        return "new_employees";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute("employee") Employee emp
+                               ) {
+
+        empServ.addEmployee(emp);
+        return "redirect:/";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable String id, Model model) {
+        Employee emp = empServ.getEmployeeById(id);
+        model.addAttribute("employee", emp);
+        return "update_employee";
+    }
+
+    @GetMapping("/deleteEmployee/{id}")
+    public String deleteEmpById(@PathVariable String id) {
+        empServ.deleteEmployeeById(id);
+        return "redirect:/";
+    }
+
+    @GetMapping("/employeeDetails/{id}")
+    public String showEmployeeDetails(@PathVariable String id, Model model) {
+        Employee employee = empServ.getEmployeeById(id);
+        model.addAttribute("employee", employee);
+        return "employee_details";
+    }
+}
---
+++ src/main/java/com/employee/management/model/Employee.java
@@ -0,0 +1,62 @@
+package com.employee.management.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @Column(name = "emp_id")
+    private String id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "dob")
+    private LocalDate dob;
+
+    @Column(name = "joined_at")
+    private LocalDateTime joinedAt;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+}
